29aff1888d8c35ee8605fc74606440f06a0636e0
Raul A. Reyes Hueros <raul.reyes.hueros@gmail.com>
2022-01-27 16:08:37 -0500

    Step4 is now ready

--- ExtractBarcodes/Scripts/Step4_optional.py
+++ ExtractBarcodes/Scripts/Step4_optional.py
@@ -1,31 +1,64 @@
-# Script will take in gDNA outputs from sc_outputs and analyze them
+#Script will take in gDNA outputs from sc_outputs and analyze them
 
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
-
 import jstyleson
 import glob
-from matplotlib_venn import venn3
 import json
 import warnings
-
 import pandas as pd
-
-#---------- User ---------------
-path_to_feature_ref = "/Volumes/GoogleDrive/My Drive/Box_20220118/Projects/Proliferative_Senesent/Data/Split_barcode/starcode_outputs/FeatureReference_filtered.csv"
-
-path_to_sc_output_folder = "/Volumes/GoogleDrive/My Drive/Box_20220118/Projects/Proliferative_Senesent/Data/Split_barcode/starcode_outputs"
-
-output_folder = "/Users/raul/Desktop/"
-
-strtseq = "GCTGTACAAGTAGGAT"
-
-GSAMP = [["RAR2","RAR2_1","RAR2_2"],["RAR3","RAR3_1","RAR3_2"]]
+import itertools
 
 
-#------------------------------
+
+#----------------load variables from paths_and_variables.json file----------------------
+#---------------------------------------------------------------------------------------
+
+#define funtion to determine if folder exist
+def does_folder_exist(path_to_folder):
+    if not os.path.exists(path_to_folder):
+        os.mkdir(path_to_folder)
+
+
+# Find paths_and_variables.json file
+path_to_script = os.path.abspath(os.getcwd())
+
+path = path = os.path.expanduser(path_to_script + "/paths_and_variables.json")
+
+# read paths_and_variables.json file
+with open(path, 'r') as myfile:
+    data=myfile.read()
+
+result_dict =  jstyleson.loads(data) # Raise Exception
+
+Outfolder= result_dict['Outfolder']    #folder you want outputs go go into (dont make this folder, this scipt will make it)
+GSAMP= result_dict['GSAMP']            #Define which samples should be run together in starcode
+
+spike_in_added= result_dict['spike_in_added']#"yes" = Spike-ins added "no" = Spike-ins not added
+spike_in_color= result_dict['spike_in_color']#List of colors for each spike-in
+spike_in_value= result_dict['spike_in_value']#List of number of cells for each spike-in
+
+
+#define any new paths
+barcode_quantify_folder =  Outfolder + "/barcode_quantify/"
+path_to_feature_ref =      Outfolder + "/CellRanger_inputs/" + "FeatureReference_filtered.csv" 
+path_to_sc_output_files = Outfolder + "/starcode_outputs/"+ "sc_output_counts__*.txt"
+
+# Add starcode to PATH
+os.environ["PATH"] += os.pathsep + scripts + '/starcode/'
+
+# Make any necessary folders
+path_to_folders = [barcode_quantify_folder]
+
+# checking whether folder/directory exists
+for path_to_folder in path_to_folders:
+    does_folder_exist(path_to_folder)
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
 
 
 
@@ -34,7 +67,7 @@ print(" ")
 
 
 #get all gDNA Read1 fastq file paths
-all_sc_output = glob.glob(path_to_sc_output_folder + "/sc_output_counts_*.txt", recursive = True)
+all_sc_output = glob.glob(path_to_sc_output_files, recursive = True)
 
 counter = 0
 for grp in GSAMP:
@@ -45,7 +78,7 @@ for grp in GSAMP:
 
     	hold_path = []
     	for path in all_sc_output:
-    		if smp+"_S" in path:                #is this true
+    		if smp in path:                #is this true
     			subset_sc_output.append(path)
 
 
@@ -71,28 +104,293 @@ for grp in GSAMP:
     	df_ref[path.split('/')[-1]] = hold_zero
 
     counter += 1
-    filepath = output_folder + "/" + '/FeatureReference_filtered_group'+ str(counter) + '_counts.csv'
+    filepath = barcode_quantify_folder +  'FeatureReference_filtered_group'+ str(counter) + '_counts.csv'
     df_ref.to_csv(filepath)  
+
+
+
+
+#---------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------
+
+
+
+
+
+print("     FeatureReference created ")
+print(" ")
+
+
+
+# Reads per million
+def rpm_norm(reads):
+    scaling_factor = np.nansum(reads)/1000000
+    return [i/scaling_factor for i in reads]
+
+def rpm_to_cells(m,b,rpm):
+    return [k*m +b for k in rpm]
+
+def get_r2_numpy(x, y):
+    slope, intercept = np.polyfit(x, y, 1)
+    r_squared = 1 - (sum((y - (slope * x + intercept))**2) / ((len(y) - 1) * np.var(y, ddof=1)))
+    return slope, intercept, r_squared
+
+#---------------------------------------Plots---------------------------------------------------
+
+counter = 0
+for grp in GSAMP:
+
+    counter += 1
+    print("     group"+ str(counter) +' :')
+    print(" ")
+    path_to_group_folder = barcode_quantify_folder + "plots_group"+ str(counter) + "/"
+    os.mkdir(path_to_group_folder)
+
+
+    #Load in Data
+    filepath = barcode_quantify_folder  + '/FeatureReference_filtered_group'+ str(counter) + '_counts.csv'
+    df_ref = pd.read_csv(filepath)
+
+    #----------------------------Find and label spike ins------------------
+    df_ref['spike_in_c'] = ['k']*len(df_ref.iloc[:,0])
+
+    if spike_in_added == 'yes':
+        for sp,spike_in_seq in enumerate(spike_in_seqs):
+            df_ref.loc[df_ref['sequence'].str.match(spike_in_seq),'spike_in_c'] = spike_in_color[sp]
+
+
+    #----------------------------Remove 0 and 1----------------------------
+    print("         Removing zeros and ones")
+    print(" ")
+
+    df_plot = df_ref.iloc[:,-len(grp)-1:]
+    df_plot = df_plot.replace(0, np.nan, regex=True)
+    df_plot = df_plot.replace(1, np.nan, regex=True)
+
+    #----------------------------Reads per million--------------------------
+    for i in range(len(grp)):
+        df_plot.iloc[:,i] = rpm_norm(df_plot.iloc[:,i])
+
+    #-------------------------Get all combinations--------------------------
+    all_combinations = itertools.combinations([g for g in range(len(grp))],2)
+
+
+    #-----------------------------------------------------------------------
+    #----------------------------Spike-ins ---------------------------------
+    if spike_in_added== 'yes':
+        #Keep Spike-ins color
+        all_spike_in_color_in_grp = []
+        all_spike_in_value_in_grp = []
+        all_line_fit = []
+
+        print("             ")
+        with PdfPages(path_to_group_folder + 'Spike_in_group'+ str(counter)+'.pdf') as pdf:
+            for i in range(len(grp)):
+
+                print("                   Spike - "  + grp[i])
+                plot_f = plt.figure()
+
+                y = df_plot.iloc[:,i] 
+                col_y = df_plot.iloc[:,-1]
+
+                new_y =[]
+                new_col_y =[]
+                for n,m in enumerate(col_y):
+                    if m in spike_in_color:
+                        #Clean up any possibly lost barcodes
+                        if np.isnan(y[n]) == False:
+                            new_y.append(y[n])
+                            new_col_y.append(m)
+
+
+
+                #Make the order of spike-ins found the same as the spike_in_values
+                x = new_col_y
+                test_list = list(zip( x,range(len(x))))
+                sort_order = spike_in_color # initializing sort order
+                test_list.sort(key = lambda i: sort_order.index(i[0])) 
+
+                new_y = [new_y[a] for _,a in test_list]
+
+                #Remove colors that had a nan
+                new_spike_in_color = []
+                new_spike_in_value = []
+                remove_new_col_y = new_col_y
+                for c,col in enumerate(spike_in_color):
+                    if col in remove_new_col_y:
+                        remove_new_col_y.remove(col)
+
+                        new_spike_in_color.append(col)
+                        new_spike_in_value.append(spike_in_value[c])
+
+                #Fit line
+                m, b = np.polyfit(new_y,new_spike_in_value, 1)
+
+                all_line_fit.append([m,b])
+
+                x_line = np.linspace(0, max(new_y), 2)
+                y_line = [k*m+b for k in x_line]
+
+
+                #Plot
+                scatt = plt.scatter(new_y,new_spike_in_value, s=3, c=new_spike_in_color)
+                
+                plt.suptitle('Group'+ str(counter), y=0.99);
+                plt.title(grp[i])
+                plt.xlabel('[RPM]')
+                plt.ylabel('[Cells]')
+                plt.plot(x_line,y_line,'-',color = 'k', alpha=0.2)
+
+
+                for c,cop in enumerate(new_spike_in_color):
+                    plt.plot([], [], '.'+cop, label=new_spike_in_value[c])
+                plt.legend(title="Spike-ins",bbox_to_anchor=(1.05, 1.0), loc='upper left')
+
+                plt.tight_layout()
+
+                #Save
+                pdf.savefig(plot_f);
+                plt.close()
+
+                all_spike_in_color_in_grp.append(new_spike_in_color)
+                all_spike_in_value_in_grp.append(new_spike_in_value)
+
+
+    #------------------------------------------------------------------------------------
+    #----------------------------Barplot-------------------------------------------------             
+    print("             Plot:")
+    with PdfPages(path_to_group_folder + 'barplot_group'+ str(counter)+'.pdf') as pdf:
+
+        
+        for i in range(len(grp)):
+
+            print("                   Bar - " + grp[i])
+            plot_f = plt.figure()
+
+            y = df_plot.iloc[:,i]
+            col_y = df_plot.iloc[:,-1]
+
+            new_y =[]
+            new_col_y =[]
+            for x,m in enumerate(y):
+                if np.isnan(m) == False:
+                    new_y.append(m)
+                    new_col_y.append(col_y[x])
+
+            if spike_in_added == 'yes':
+                #Convert from RPM to Cells
+                m,b = all_line_fit[i]
+                new_y = [k*m for k in new_y]
+
+            #Order
+            index_y = sorted(range(len(new_y)), reverse=True,key=new_y.__getitem__)
+            new_col_y = [new_col_y[k] for k in index_y]
+            new_y = sorted(new_y, reverse=True)
+
+            #Plot
+            plt.bar(range(len(new_y)),new_y, color=new_col_y)
+            plt.suptitle('Group'+ str(counter), y=0.99);
+            plt.title(grp[i])
+            plt.xlabel('Barcodes')
+            plt.ylabel('Cells')
+
+            if spike_in_added == 'yes':
+                new_spike_in_color = all_spike_in_color_in_grp[i]
+                new_spike_in_value = all_spike_in_value_in_grp[i]
+
+                for c,cop in enumerate(new_spike_in_color):
+                    plt.plot([], [], '.'+cop, label=new_spike_in_value[c] )
+                plt.legend(title="Spike-ins",bbox_to_anchor=(1.05, 1.0), loc='upper left')
+
+            plt.tight_layout()
+
+            #Save
+            pdf.savefig(plot_f);
+            plt.close()
+
+ 
+    
+    #---------------------------------------------------------------------
+    #----------------------------Scatter plot ----------------------------
+            
+    print("             ")
+    with PdfPages(path_to_group_folder+ 'scatter_group'+ str(counter)+'.pdf') as pdf:
+
+        
+        for combination in all_combinations:
+
+            print("                   Scatt - " + grp[combination[0]] + " and " + grp[combination[1]])
+            plot_f = plt.figure()
+
+            x = df_plot.iloc[:,combination[0]] 
+            y = df_plot.iloc[:,combination[1]] 
+            col_y = df_plot.iloc[:,-1]
+
+            if spike_in_added == 'yes':
+                #Convert from RPM to Cells
+                m_x,b_x = all_line_fit[combination[0]]
+                x = rpm_to_cells(m_x,0,x)
+
+                m_y,b_y = all_line_fit[combination[0]]
+                y = rpm_to_cells(m_y,0,y)
+
+            
+            new_x =[]
+            new_y =[]
+            no_pike_x = []
+            no_pike_y = []
+            new_col_y =[]
+            for i,m in enumerate(x):
+                if np.isnan(m) == False:
+                    if np.isnan(y[i]) == False:
+
+
+                        new_x.append(m)
+                        new_y.append(y[i])
+                        new_col_y.append(col_y[i])
+
+                        if col_y[i] == 'k':    # without spike in
+                            no_pike_x.append(m)
+                            no_pike_y.append(y[i])
+
+            # fit line and get r^2
+            slope, intercept, r_squared = get_r2_numpy(np.array(no_pike_x), np.array(no_pike_y) ) 
+
+            x_fit = [0,max(no_pike_x)]
+            y_fit =[k*slope +intercept for k in x_fit]
+
+            #Plot
+            plt.scatter(new_x,new_y, s=3, c=new_col_y)
+            plt.plot(x_fit,y_fit,'-',color = 'k', alpha=0.2)
+            plt.suptitle('Group'+ str(counter), y=0.99);
+            plt.title(grp[combination[0]] + " and " + grp[combination[1]])
+            plt.xlabel(grp[combination[0]] + '[Cells]')
+            plt.ylabel(grp[combination[1]] + '[Cells]')
+
+            fit_name = "R^2=" +"{:.2f}".format(r_squared) + "     slope=" + "{:.2f}".format(slope)+ "     y-intercept=" + "{:.2f}".format(intercept)
+
+            plt.text(0,np.max(new_y),fit_name)
+
+            if spike_in_added == 'yes':
+                for c,cop in enumerate(spike_in_color ):
+                    plt.plot([], [], '.'+cop, label=spike_in_value[c])
+                plt.legend(title="Spike-ins",bbox_to_anchor=(1.05, 1.0), loc='upper left')
+
+            plt.tight_layout()
+
+            #Save
+            pdf.savefig(plot_f);
+            plt.close()
+
 
 
 
 print("     Done :D ")
 print(" ")
 
-#---------plot-----------
 
-# plot_f = plt.figure()
-# set1 = set(seq_base_run_no)
-# set2 = set(fuzzy_no[cnt_i])
-# set3 = set(qscore_no)
 
-# ax = plt.gca() 
 
-# total = len(set1.union(set2,set3))
 
-# venn3([set1, set2,set3], ('Base_repeat', 'Match_to_StartSeq','Qscore'),subset_label_formatter=lambda x: f"{(x/total):1.0%}");
 
-# plt.suptitle(sample + "_venn" +  "   pct_startseq_match=" + str(fuzz_i), y=0.99);
-# plt.title("Percent of total reads: Qscore=" + str("{:3.0f}".format(100*len(qscore_no)/len(seqs))) + " Base_repeat=" + str("{:3.0f}".format(100*len(seq_base_run_no)/len(seqs))) + " Match_to_StartSeq=" + str("{:3.0f}".format(100*len(fuzzy_no[cnt_i])/len(seqs))) );
-# pdf.savefig(plot_f);
-# plt.close()
+
